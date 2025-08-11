4fb9f75fb193e384d38d3aecb5c6128de905f4af
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-11 13:08:09 +0900

    [Add] 카테고리 값 넣기 - json 추가

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
 $(function () {
   /*
-    해야 하는 것!!
-    1. 메뉴 슬라이드
+    넣어야하는 값!!
+    1. 배너 슬라이드 값 넣기
+    2. 카테고리 값
+    3. 상품 값
+    4. 콘텐츠 값
+    5. 편성표 값
+  */
+  /*
+    해야 하는 기능!!
+    1. 메뉴 슬라이드 기능
         - 3초에 한 번씩 자동 돌아가기 (반복) : 페이지가 열리면 바로 시작할 것
         - 이전 다음 선택 시 상황에 맞게 이동되어질 것
         - 오 -> 왼으로 이동되면서 변경 : 반복되어 보여야 함!!
+    2. 
   */
+
+  // 배너 슬라이드 임시 반복
   $(".banner-item").slice(0, 3).clone().appendTo("#bannerList");
+
+  // 배너 슬라이드 실행
   swiper();
+
+  // 카테고리 등록
+  addCategory();
 });
 
+// 배너 슬라이드 실행
 function swiper() {
   let currentPage = 0;
   const bannerList = document.querySelector("#bannerList");
@@ -78,4 +95,28 @@ function swiper() {
       }
     });
   }
+}
+// 카테고리 등록
+function addCategory() {
+  $.get("../json/category.json").done(function (data) {
+    console.log(data);
+    if (data) {
+      $("#cateResult").html(
+        data.map(
+          (i) => `
+            <li class="category-item" >
+                <a href="${i.link}">
+                    <div class="cate-icon">
+                        <img src="${i.image}" alt="${i.alt}" />
+                    </div>
+                    <p class="cate-name">${i.title}</p>
+                </a>
+            </li>
+            `
+        )
+      );
+
+      console.log(document.getElementById("cateResult"));
+    }
+  });
 }
