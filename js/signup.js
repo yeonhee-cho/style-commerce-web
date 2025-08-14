71529b54511619369ebe6e225eb1127c0d74db5e
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-14 16:25:52 +0900

    [Add] 회원가입

--- js/signup.js
+++ js/signup.js
@@ -3,6 +3,7 @@ $(function () {
   pwCheckFn();
   nameCheckFn();
   emailCheckFn();
+  agreeCheck();
 
   $("#signupBtn").click(function (e) {
     e.preventDefault();
@@ -29,6 +30,7 @@ const idResult = document.getElementById("idResult");
 const passwordResult = document.getElementById("passwordResult");
 const nameResult = document.getElementById("nameResult");
 const emailResult = document.getElementById("emailResult");
+const agreeResult = document.getElementById("agreeResult");
 
 // 유효성 검사 - 비밀번호
 function isValidPassword(pw) {
@@ -89,7 +91,7 @@ function pwCheckFn() {
     } else if (passwordCheckValue && passwordValue !== passwordCheckValue) {
       passwordResult.innerHTML = `<p class="validation red">비밀번호가 일치하지 않습니다.</p>`;
     } else {
-      passwordCheckResult.innerHTML = `<p class="validation green">비밀번호가 일치합니다.</p>`;
+      passwordResult.innerHTML = `<p class="validation green">비밀번호가 일치합니다.</p>`;
     }
   });
 }
@@ -115,7 +117,7 @@ function nameCheckFn() {
 function emailCheckFn() {
   userEmailInput.addEventListener("input", () => {
     const userEmailValue = userEmailInput.value.trim();
-    if (!userEmailInput) {
+    if (!userEmailValue) {
       emailResult.innerHTML = `<p class="validation red">이메일을 입력해 주세요.</p>`;
     } else if (!validateEmail(userEmailValue)) {
       emailResult.innerHTML = `<p class="validation red">이메일 형식에 맞지 않습니다. 다시 한 번 확인해주세요.</p>`;
@@ -125,21 +127,145 @@ function emailCheckFn() {
   });
 }
 
-//
+// 오류 시 이동
+function goIdInput() {
+  userIdInput.focus();
+  userIdInput.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+  });
+}
+function goPwInput() {
+  passwordInput.focus();
+  passwordInput.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+  });
+}
+function goNameInput() {
+  userNameInput.focus();
+  userNameInput.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+  });
+}
+function goEmailInput() {
+  userEmailInput.focus();
+  userEmailInput.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+  });
+}
+
+// 회원가입 데이터 전송
 function signupFn() {
+  const userIdValue = userIdInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+  const passwordCheckValue = passwordCheckInput.value.trim();
+  const userNameValue = userNameInput.value.trim();
+  const userEmailValue = userEmailInput.value.trim();
+  const recommenderValue = recommenderInput.value.trim();
+
+  const users = JSON.parse(localStorage.getItem("userList") || "[]");
+
+  // 유효성 체크
+  if (!userIdValue) {
+    alert("아이디를 입력해 주세요.");
+    goIdInput();
+    return;
+  }
+  if (users.some((u) => u.userId === userIdValue)) {
+    alert("이미 사용 중인 아이디입니다.");
+    goIdInput();
+    return;
+  }
+  if (!passwordValue || !isValidPassword(passwordValue)) {
+    alert("비밀번호 조건을 확인해 주세요.");
+    goPwInput();
+    return;
+  }
+  if (passwordValue !== passwordCheckValue) {
+    alert("비밀번호가 일치하지 않습니다.");
+    goPwInput();
+    return;
+  }
+  if (
+    !userNameValue ||
+    !isValidName(userNameValue) ||
+    userNameValue.length < 2 ||
+    userNameValue.length > 10
+  ) {
+    alert("이름 조건을 확인해 주세요.");
+    goNameInput();
+    return;
+  }
+  if (!userEmailValue || !validateEmail(userEmailValue)) {
+    alert("이메일 형식을 확인해 주세요.");
+    goEmailInput();
+    return;
+  }
+
+  const agreePersonal = document.getElementById("agreePersonal");
+  const agreeStore = document.getElementById("agreeStore");
+  console.log(agreePersonal.checked, agreeStore.checked);
+
+  if (!agreePersonal.checked) {
+    agreeResult.innerHTML = `<p class="validation red">
+                                  개인정보 수집 및 이용 동의가 필요합니다.
+                                </p>`;
+    agreePersonal.focus();
+    agreePersonal.scrollIntoView({ behavior: "smooth", block: "center" });
+    return;
+  }
+  if (!agreeStore.checked) {
+    agreeResult.innerHTML = `<p class="validation red">
+                              스토어 이용약관 동의가 필요합니다.
+                            </p>`;
+    agreeStore.focus();
+    agreeStore.scrollIntoView({ behavior: "smooth", block: "center" });
+    return;
+  }
+
   const newUser = {
-    userId: userId,
-    password: password,
-    passwordCheck: passwordCheck,
-    userName: userName,
-    userEmail: userEmail,
-    recommender: recommender,
+    userId: userIdValue,
+    password: passwordValue,
+    passwordCheck: passwordCheckValue,
+    userName: userNameValue,
+    userEmail: userEmailValue,
+    recommender: recommenderValue,
   };
+
   users.push(newUser);
   localStorage.setItem("userList", JSON.stringify(users));
   sessionStorage.setItem("loggedInUser", JSON.stringify(newUser));
-  // window.location.href = "/";
-  // alert("회원가입이 완료되었습니다.");
+
+  setTimeout(() => {
+    window.location.href = "/";
+    alert("회원가입이 완료되었습니다.");
+  }, 50);
+}
+
+function agreeCheck() {
+  const agreeAll = document.getElementById("agreeAll");
+
+  const agreeItems = document.querySelectorAll(
+    "#agreePersonal, #agreeStore, #agreeMarketing"
+  );
+
+  // 전체 동의 클릭 시 -> 나머지 체크박스 전부 선택/해제
+  agreeAll.addEventListener("change", () => {
+    agreeItems.forEach((checkbox) => {
+      checkbox.checked = agreeAll.checked;
+    });
+  });
+
+  // 개별 체크박스 변경 시 -> 전체 동의 상태 업데이트
+  agreeItems.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      const allChecked = Array.from(agreeItems).every((cb) => cb.checked);
+      agreeAll.checked = allChecked;
+    });
+  });
 }
 
 // 개인정보 수집 및 이용 동의
