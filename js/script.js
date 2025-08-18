8bcc870f6e8ff1f1e10f0b15934c59a0d5acbd91
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-18 22:37:33 +0900

    [Add] 공지사항

--- js/script.js
+++ js/script.js
@@ -60,7 +60,10 @@ window.addEventListener("load", async () => {
             currentPath.includes("signup.html") ||
             currentPath.includes("product-detail.html") ||
             currentPath.includes("/search-detail.html") ||
-            currentPath.includes("/mypage.html")
+            currentPath.includes("/mypage.html") ||
+            currentPath.includes("/notice.html") ||
+            currentPath.includes("/faq.html") ||
+            currentPath.includes("/inquiry.html")
           ) {
             const search = tempDiv.querySelector(".search-header");
             const segment = tempDiv.querySelector(".segment-header");
